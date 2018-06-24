6adfd7b1348de3cc69f4ed1cecc348b84a71e587
hpablom <pablo.hernandez@navent.com>
2018-06-24 17:59:00 -0300

    fix data pbi

--- data/preprocess_data.r
+++ data/preprocess_data.r
@@ -138,10 +138,10 @@ View(dbGetQuery(conn=db, " select * from underground where station_id = 100"))
 
 data_to_export2 = dbGetQuery(conn=db, "
 select 
- city_country, 
- y.year, 
- coalesce( cast(pbi.pbi as number), 0),
- coalesce(sum(u.station_meters/ (u.station_opening - u.station_buildstart) ), 0)
+ city_country country, 
+ y.year year, 
+ coalesce( cast(pbi.pbi as number), 0) pbi,
+ coalesce(sum(u.station_meters/ (u.station_opening - u.station_buildstart) ), 0) meters
  from YEARS y
  join pbi on y.year = pbi.year
  left join underground u on pbi.country = u.city_country and y.year between u.station_buildstart and u.station_opening
